204615f2268e163e557177e69431dea50343e495
Keeyong <keeyonghan@hotmail.com>
2025-09-13 15:55:36 -0700

    Update ML-Forecasting-Against-Stock-Data.sql

--- week4/ML-Forecasting-Against-Stock-Data.sql
+++ week4/ML-Forecasting-Against-Stock-Data.sql
@@ -1,13 +1,48 @@
--- This is your Cortex Project.
--- https://quickstarts.snowflake.com/guide/getting-started-with-snowflake-cortex-ml-forecasting-and-classification/index.html
--- dev.raw_data.market_data is already populated by a data pipeline
------------------------------------------------------------
--- SETUP
------------------------------------------------------------
-use role ACCOUNTADMIN;
-use warehouse COMPUTE_WH;
-use database DEV;
-use schema RAW_DATA;
+CREATE DATABASE USER_DB_RACCOON;  -- CHANGE THIS ACCORDING TO YOUR DATABASE
+CREATE SCHEMA raw;
+CREATE SCHEMA analytics;
+
+-- Create a table
+CREATE TABLE IF NOT EXISTS MARKET_DATA (
+    DATE DATE NOT NULL,
+    OPEN FLOAT NOT NULL,
+    HIGH FLOAT NOT NULL,
+    LOW FLOAT NOT NULL,
+    CLOSE FLOAT NOT NULL,
+    VOLUME BIGINT NOT NULL,
+    SYMBOL VARCHAR(10) NOT NULL,
+    CREATED_AT TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
+
+CREATE OR REPLACE FILE FORMAT csv_format
+    TYPE = 'CSV'
+    FIELD_DELIMITER = ','
+    RECORD_DELIMITER = '\n'
+    SKIP_HEADER = 1              -- 헤더 라인 건너뛰기
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    TRIM_SPACE = TRUE
+    ERROR_ON_COLUMN_COUNT_MISMATCH = TRUE
+    DATE_FORMAT = 'YYYY-MM-DD';
+
+CREATE OR REPLACE STAGE tesla_stage
+    FILE_FORMAT = csv_format;
+
+-- Upload the file to the stage using the UI
+-- The file is on CANVAS
+
+-- Check the file under the stage
+LIST @tesla_stage;
+
+COPY INTO MARKET_DATA (DATE, OPEN, HIGH, LOW, CLOSE, VOLUME, SYMBOL)
+FROM @tesla_stage
+FILE_FORMAT = csv_format;
+
+SELECT 
+    MIN(DATE) as START_DATE,
+    MAX(DATE) as END_DATE,
+    COUNT(*) as RECORD_COUNT
+FROM MARKET_DATA 
+WHERE SYMBOL = 'TSLA';
 
 -- Inspect the first 10 rows of your training data. This is the data we'll use to create your model.
 select * from MARKET_DATA limit 10;
@@ -19,9 +54,6 @@ CREATE VIEW MARKET_DATA_v1 AS SELECT
     SYMBOL
 FROM MARKET_DATA;
 
------------------------------------------------------------
--- CREATE PREDICTIONS
------------------------------------------------------------
 -- Create your model.
 CREATE SNOWFLAKE.ML.FORECAST stock_price(
     INPUT_DATA => SYSTEM$REFERENCE('VIEW', 'MARKET_DATA_v1'),
@@ -35,80 +67,21 @@ CREATE SNOWFLAKE.ML.FORECAST stock_price(
 BEGIN
     -- This is the step that creates your predictions.
     CALL stock_price!FORECAST(
-        FORECASTING_PERIODS => 7,
+        FORECASTING_PERIODS => 14,
         -- Here we set your prediction interval.
         CONFIG_OBJECT => {'prediction_interval': 0.95}
     );
     -- These steps store your predictions to a table.
     LET x := SQLID;
-    CREATE TABLE My_forecasts_2024_09_10 AS SELECT * FROM TABLE(RESULT_SCAN(:x));
+    CREATE TABLE My_forecasts_2025_09_16 AS SELECT * FROM TABLE(RESULT_SCAN(:x));
 END;
 
 -- View your predictions.
-SELECT * FROM My_forecasts_2024_09_10;
+SELECT * FROM My_forecasts_2025_09_16;
 
 -- Union your predictions with your historical data, then view the results in a chart.
 SELECT SYMBOL, DATE, CLOSE AS actual, NULL AS forecast, NULL AS lower_bound, NULL AS upper_bound
     FROM MARKET_DATA
 UNION ALL
 SELECT replace(series, '"', '') as SYMBOL, ts as DATE, NULL AS actual, forecast, lower_bound, upper_bound
-    FROM My_forecasts_2024_09_10;
-
------------------------------------------------------------
--- INSPECT RESULTS
------------------------------------------------------------
-
--- Inspect the accuracy metrics of your model. 
-CALL stock_price!SHOW_EVALUATION_METRICS();
-
--- Inspect the relative importance of your features, including auto-generated features. 
-CALL stock_price!EXPLAIN_FEATURE_IMPORTANCE();
-
------------------------------------------------------------
--- AUTOMATION OF FORECASTING
------------------------------------------------------------
--- Uncaught exception of type 'STATEMENT_ERROR' on line 2 at position 8 : SQL compilation error: Unknown function FORECAST_SUBSCRIPTIONS_MODEL!FORECAST
--- Update your input data with recent data to make your predictions as accurate as possible. 
-CREATE OR REPLACE TASK train_task
-WAREHOUSE = "COMPUTE_WH"
-SCHEDULE = 'USING CRON 0 0 * * 1  America/Los_Angeles' -- Runs at midnight PT, Monday morning.
-AS
-    CREATE OR REPLACE SNOWFLAKE.ML.FORECAST forecast_subscriptions_model(
-        INPUT_DATA => SYSTEM$REFERENCE('VIEW', 'MARKET_DATA_V1'),
-        SERIES_COLNAME => 'SYMBOL',
-        TIMESTAMP_COLNAME => 'DATE_V1',
-        TARGET_COLNAME => 'CLOSE'
-    );
-
--- Unknown function FORECAST_SUBSCRIPTIONS_MODEL!FORECAST
--- FORECAST interface: model created without exogenous features. Please use the forecasting_periods argument to generate a forecast.
-CREATE OR REPLACE TASK predict_task
-WAREHOUSE = "COMPUTE_WH"
-SCHEDULE = 'USING CRON 0 1 * * 1 America/Los_Angeles' -- Runs at 1am PT, Monday morning.
-AS
-    BEGIN
-        CALL forecast_subscriptions_model!FORECAST(
-            FORECASTING_PERIODS => 7,
-            -- Here we set your prediction interval.
-            CONFIG_OBJECT => {'prediction_interval': 0.95},
-            INPUT_DATA => SYSTEM$REFERENCE('VIEW', 'MARKET_DATA_V1'),
-            SERIES_COLNAME => 'SYMBOL',
-            TIMESTAMP_COLNAME => 'DATE_V1'
-        );
-        LET x := SQLID;
-        CREATE OR REPLACE TABLE my_forecasts AS SELECT * FROM TABLE(RESULT_SCAN(:x));
-    END; 
-
--- Execute your task immediately to confirm it is working. 
-EXECUTE TASK train_task;
-EXECUTE TASK predict_task;
-
-SHOW TASKS;
-
--- View your predictions.
-SELECT * FROM my_forecasts;
-
--- Suspend or drop your task.
--- ALTER TASK train_task suspend;
--- DROP TASK train_task;
--- DROP TASK predict_task;
+    FROM My_forecasts_2025_09_16;
